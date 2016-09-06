73945739d6592e09c801a6388575a4c847779d5a
wiichog <gar14189@uvg.edu.gt>
2016-09-06 11:06:55 -0600

    Project#2 fixinf some troubles in exercise #3

--- Homework/MiniProyecto2/Ejercicio3.py
+++ Homework/MiniProyecto2/Ejercicio3.py
@@ -7,42 +7,48 @@ Created on Mon Sep 05 18:30:30 2016
 import random
 from math import *
 
-def Normal(M,Sig):#esti es para calcular una distribucion normal
+def Normal(M,Sig):#algoritmo dado en clase
     while True:
-        Y1 = random.random()#generamos aleatorio
-        Y2 = random.random()#generamos aleatorios
-        if (Y2-((Y1-1)**2))/2 >0:#calculamos una exponencial
+        Y1 = exp(1)#generamos un random con distribucion exponencial
+        Y2 = exp(1)#generamos random con distribucion exponencial
+        if (Y2-(Y1-1)**2)/2 > 0:#calculamos una exponencial
             U = random.random()#calculamos otro random
-            if U<= 0.05:#si ese random esta por debajo de la media
-                return M + (Sig*Y1)
+            if U <= 0.5:#si ese random esta por debajo de la media
+                return M + Sig*Y1
             else:
-                return M - (Sig*Y1)
+                return M - Sig*Y1
 
-def e(X, lam): return -(1/lam)*(ln(X))#definimos para la exponencial
+
+
+def exp(lam): return -(1/lam)*log(random.random())#definimos para la exponencial
+
+
     
 def uniforme(a,b):#deifinimos para la uniforme
     x = random.random()#declaramos random
-    r = (b-a)*(x+a)#calculamos uniforme
+    r = (b-a)*x+a#calculamos uniforme
     return r#retornamos la r
 
+
+
 def ValorNeto(V,t):#funcion del valor neto 
-    r = V[0]
-    for i in range(len(V)):
-        r += V[i]/(1+t)**7
-    return r       
+   r = V[0]
+   for i in range(len(V)):
+       r = r +  V[i]/((1+t)**i)
+   return r   
         
 t= 0.10
-NoIteraciones = 10000
 VNHotel = VnComercial = 0
-Lista1 = [-800,Normal(-800,50),Normal(-800,100),Normal(-700,150),Normal(300,200),Normal(400,200),Normal(500,200),uniforme(200,8440)]
-Lista2 = [-900,Normal(-600,50),Normal(-200,50),Normal(-600,100),Normal(250,150),Normal(350,150),Normal(400,150),uniforme(1600,1600)]
-for i in range(0,NoIteraciones):          
-    VNHotel = ValorNeto(Lista1,t)
-    VnComercial = ValorNeto(Lista2,t)
+numero = 10
+for i in range(0,numero):
+    Lista1 = [-800, Normal(-800,50), Normal(-800,100), Normal(-700,150), Normal(300,200), Normal(400,200), Normal(500,200), uniforme(200,8440)]
+    Lista2 = [-900,Normal(-600,50),Normal(-200,50),Normal(-600,100),Normal(250,150),Normal(350,150),Normal(400,150),uniforme(1600,6000)]     
+    VNHotel = VNHotel + ValorNeto(Lista1,t)
+    VnComercial = VnComercial + ValorNeto(Lista2,t)
 
 
-print ("El valor neto del Hotel es de: "+ str(VNHotel))
-print ("El valor neto del Centro Comercial es de: "+ str(VnComercial))
+print ("El valor neto del Hotel es de: "+ str(VNHotel/numero))
+print ("El valor neto del Centro Comercial es de: "+ str(VnComercial/numero))
 
 if VnComercial > VNHotel:
     print ("Se deberia de invertir en el centro comercial")
