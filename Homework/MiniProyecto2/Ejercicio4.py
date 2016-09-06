7a68f602062f971fef087cac14ac46efe70133cc
wiichog <gar14189@uvg.edu.gt>
2016-09-06 15:44:33 -0600

    Final changes

--- Homework/MiniProyecto2/Ejercicio4.py
+++ Homework/MiniProyecto2/Ejercicio4.py
@@ -11,16 +11,19 @@ def CompraPeriodicos(Tiempo,NumeroPeriodicos,NumeroPeriodicosTotal):
     for i in range(0,Tiempo):
         x = random.random()
         if (x<0.30):#Para probalbilidad debajo de 30%
-            CompraInicial = CompraInicial + (2.5*NumeroPeriodicos)#Le sumamos la compra inicial 
+            CompraInicial = CompraInicial + (2.5*NumeroPeriodicos)#Suma de ganancia
         elif (0.30<x<=0.70):
-            CompraInicial = CompraInicial + (2.5*NumeroPeriodicos)
+            CompraInicial = CompraInicial + (2.5*NumeroPeriodicos)#Suma de ganancia
             CompraInicial = CompraInicial + 0.50*(NumeroPeriodicosTotal-NumeroPeriodicos)#reembolso
         else:
-            CompraInicial = CompraInicial + (2.5*NumeroPeriodicos)
+            CompraInicial = CompraInicial + (2.5*NumeroPeriodicos)#Suma de ganancia
             CompraInicial = CompraInicial + 0.50*(NumeroPeriodicosTotal-NumeroPeriodicos)#reembolso
     return CompraInicial
-
+print ("")
 for i in range(9,12):
     print ("La ganancia para " + str(i) + " periodicos es "+ str(CompraPeriodicos(30,i,11)) + " esto es para 1 mes")
     print ("La ganancia para " + str(i) + " periodicos es "+ str(CompraPeriodicos(365,i,11)) + " esto es para 1 año")
     print ("La ganancia para " + str(i) + " periodicos es "+ str(CompraPeriodicos(3650,i,11)) + " esto es para 10 años")
+    if (i == 9): print ("")
+    if (i == 10): print ("")
+    if (i == 11): print ("")
