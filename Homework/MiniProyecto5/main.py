53186df9ef4063ad6a1c9edf2a56ba5015c00315
wiichog <gar14189@uvg.edu.gt>
2016-10-25 19:06:56 -0600

    First part is ready

--- Homework/MiniProyecto5/main.py
+++ Homework/MiniProyecto5/main.py
@@ -1,14 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 18 14:51:22 2016
-
-@author: wiichog
-"""
-
 import pygame
 import random
 import math
 
+def Draw(done,ballImage,dudeImage,ballX,ballY,dudeX,dudeY,Angle):
+        screen.fill(Green)
+        screen.blit(ballImage,(ballX,ballY))
+        pygame.draw.circle(screen,Red,(dudeX,dudeY),5,0)
+        pygame.draw.line(screen,White,(dudeXInitPos,dudeYInitPos),(dudeX+math.cos(math.radians(Angle)),dudeY+360*math.sin(math.radians(-Angle))))
+        pygame.draw.line(screen,Red,(390,100),(390,250),10)
+        pygame.display.flip()
+#def mover():
+    
+        
 def FuzzyAngle(ballAngle,dudeAngle,FuzzyStatement):
     if(ballAngle>dudeAngle and FuzzyStatement=="MuyLejos"):
         return dudeAngle + 10
@@ -22,26 +25,7 @@ def FuzzyAngle(ballAngle,dudeAngle,FuzzyStatement):
         return (dudeAngle + 1)
     elif(ballAngle<dudeAngle and FuzzyStatement=="Cerca"):
         return dudeAngle - 1
-    
-def Draw(done,ballImage,dudeImage,ballX,ballY,dudeX,dudeY):
-    
-        screen.fill(Green)
-        screen.blit(ballImage,(ballX,ballY))
-        screen.blit(dudeImage,(dudeX,dudeY))
-        pygame.display.flip()
-    
-def MoveDude(done,ballImage,dudeImage,ballX,ballY,dudeX,dudeY,Angle,FuzzyStatement):
-    if(FuzzyStatement == "MuyLejos"):
-        for i in range(10):
-            dudeX = dudeX + 1
-            dudeY = dudeY + 1
-            print "posicion X " ,abs(dudeX*math.cos(Angle)), " cos es " ,math.cos(Angle), " posicion y ",abs(dudeY*math.sin(Angle)), " sin es ", math.sin(Angle) 
-            Draw(done,ballImage,dudeImage,ballX,ballY,abs(dudeX*math.cos(Angle)),abs(dudeY*math.sin(Angle)))
-    
 
-    
-    
-    
 #This is the colors that we are gonna use
 Black = (  0,  0,  0)
 White = (255,255,255)
@@ -51,23 +35,15 @@ Red   = (255,  0,  0)
 #size keeps the size of the screen we need
 size = [400,300]
 #We throw some random number between 400 and 300 to have the figure inside the screen
-ballXInitPos = random.randint(0,400)
-dudeXInitPos = random.randint(0,400)
-ballYInitPos = random.randint(0,300)
-dudeYInitPos = random.randint(0,300)
+ballXInitPos = random.randint(0,300)
+dudeXInitPos = random.randint(0,300)
+ballYInitPos = random.randint(0,200)
+dudeYInitPos = random.randint(0,200)
 #we throw another random between 0 and 360 for the angle of the dude
 dudeAngleInit = random.randint(0,360)
-#we calculate the distance between the ball and the dude
 Distance = round(math.sqrt((ballXInitPos-dudeXInitPos)**2+(ballYInitPos-dudeYInitPos)**2),2)
-#we calculate the angle to have the first condition if to have the complement of the angle and the else is to calculate the normal angle
-if(ballYInitPos>dudeYInitPos):
-    Angulo = round(360 - abs(math.degrees(math.atan2((ballYInitPos-dudeYInitPos),(ballXInitPos-dudeXInitPos)))),0)
-else:
-    Angulo = round(abs(math.degrees(math.atan2((ballYInitPos-dudeYInitPos),(ballXInitPos-dudeXInitPos)))),0)
-
-
-        
 screen = pygame.display.set_mode(size)
+
 pygame.display.set_caption("Juan Luis Garcia 14189")
 #Vamos a utilizar done y clock para controlar cuando el usuario quiera cerrar la pantalla
 done = False
@@ -75,20 +51,38 @@ clock = pygame.time.Clock()
 ballImage = pygame.image.load('pelota.png')
 dudeImage = pygame.image.load('personaje.png')
 
-while not done:
-        clock.tick(10)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            Draw(done,ballImage,dudeImage,ballXInitPos,ballYInitPos,dudeXInitPos,dudeYInitPos)
-            MoveDude(done,ballImage,dudeImage,ballXInitPos,ballYInitPos,dudeXInitPos,dudeYInitPos,dudeAngleInit,"MuyLejos")
+if(ballYInitPos>dudeYInitPos):
+    AngleBallAndDude = round(360 - abs(math.degrees(math.atan2((ballYInitPos-dudeYInitPos),(ballXInitPos-dudeXInitPos)))),0)
+else:
+    AngleBallAndDude = round(abs(math.degrees(math.atan2((ballYInitPos-dudeYInitPos),(ballXInitPos-dudeXInitPos)))),0)
 
-if(abs(Angulo-dudeAngleInit)>100):
-    dudeAngleInit=FuzzyAngle(Angulo,dudeAngleInit,"MuyLejos")
-elif(abs(Angulo-dudeAngleInit)>=50 and abs(Angulo-dudeAngleInit)<=100):
-    dudeAngleInit=FuzzyAngle(Angulo,dudeAngleInit,"MedioLejos")
-elif(abs(Angulo-dudeAngleInit)>=0 and abs(Angulo-dudeAngleInit)<=50):
-    dudeAngleInit=FuzzyAngle(Angulo,dudeAngleInit,"Cerca")
-   
-    
+contador = 0
+print "Dude ",dudeAngleInit
+print "Ball & Dude ",AngleBallAndDude
+while not done:
+    clock.tick(10)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        while contador != 1:
+            while AngleBallAndDude!=dudeAngleInit:
+                if(abs(AngleBallAndDude-dudeAngleInit)>100):
+                    dudeAngleInit=FuzzyAngle(AngleBallAndDude,dudeAngleInit,"MuyLejos")
+                    Draw(done,ballImage,dudeImage,ballXInitPos,ballYInitPos,dudeXInitPos,dudeYInitPos,dudeAngleInit)
+                elif(abs(AngleBallAndDude-dudeAngleInit)>=50 and abs(AngleBallAndDude-dudeAngleInit)<=100):
+                    dudeAngleInit=FuzzyAngle(AngleBallAndDude,dudeAngleInit,"MedioLejos")
+                    Draw(done,ballImage,dudeImage,ballXInitPos,ballYInitPos,dudeXInitPos,dudeYInitPos,dudeAngleInit)
+                elif(abs(AngleBallAndDude-dudeAngleInit)>=0 and abs(AngleBallAndDude-dudeAngleInit)<=50):
+                    dudeAngleInit=FuzzyAngle(AngleBallAndDude,dudeAngleInit,"Cerca")
+                    Draw(done,ballImage,dudeImage,ballXInitPos,ballYInitPos,dudeXInitPos,dudeYInitPos,dudeAngleInit)
+            for i in range(int(Distance)):
+                Draw(done,ballImage,dudeImage,ballXInitPos,ballYInitPos,dudeXInitPos+int(i*math.cos(math.radians(AngleBallAndDude))),dudeYInitPos+int(i*math.sin(math.radians(-AngleBallAndDude))),dudeAngleInit)
+            if(ballYInitPos>450):
+                AngleBallAndGoal = round(360-abs(math.degrees(math.atan2((ballYInitPos-450),(ballXInitPos-890)))),0) 
+            else:
+                AngleBallAndGoal = round(abs(math.degrees(math.atan2((ballYInitPos-450),(ballXInitPos-890)))),0) 
+            print "Angulo entre pelota y porteria" , AngleBallAndGoal
+            
+            contador = contador +1
+            
 pygame.quit()
